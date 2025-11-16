eacd42a9fab473fc4f92b11f4f2b29da7b08c0d6
Ola’s Laptop <olaslaptop@Olass-MacBook-Air.local>
2025-11-16 12:25:17 -0600

    not needed

---
+++ oglcreation/tutorial1/external/CMakeFiles/ANTTWEAKBAR_116_OGLCORE_GLFW.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/olaslaptop/learningss/Ccode/opengl/oglcreation/tutorial1/external/AntTweakBar-ObjectiveC/TwEventGLFW.m" "external/CMakeFiles/ANTTWEAKBAR_116_OGLCORE_GLFW.dir/AntTweakBar-ObjectiveC/TwEventGLFW.m.o" "gcc" "external/CMakeFiles/ANTTWEAKBAR_116_OGLCORE_GLFW.dir/AntTweakBar-ObjectiveC/TwEventGLFW.m.o.d"
+  "/Users/olaslaptop/learningss/Ccode/opengl/oglcreation/tutorial1/external/AntTweakBar-ObjectiveC/LoadOGL.mm" "external/CMakeFiles/ANTTWEAKBAR_116_OGLCORE_GLFW.dir/AntTweakBar-ObjectiveC/LoadOGL.mm.o" "gcc" "external/CMakeFiles/ANTTWEAKBAR_116_OGLCORE_GLFW.dir/AntTweakBar-ObjectiveC/LoadOGL.mm.o.d"
+  "/Users/olaslaptop/learningss/Ccode/opengl/oglcreation/tutorial1/external/AntTweakBar-ObjectiveC/LoadOGLCore.mm" "external/CMakeFiles/ANTTWEAKBAR_116_OGLCORE_GLFW.dir/AntTweakBar-ObjectiveC/LoadOGLCore.mm.o" "gcc" "external/CMakeFiles/ANTTWEAKBAR_116_OGLCORE_GLFW.dir/AntTweakBar-ObjectiveC/LoadOGLCore.mm.o.d"
+  "/Users/olaslaptop/learningss/Ccode/opengl/oglcreation/tutorial1/external/AntTweakBar-ObjectiveC/TwBar.mm" "external/CMakeFiles/ANTTWEAKBAR_116_OGLCORE_GLFW.dir/AntTweakBar-ObjectiveC/TwBar.mm.o" "gcc" "external/CMakeFiles/ANTTWEAKBAR_116_OGLCORE_GLFW.dir/AntTweakBar-ObjectiveC/TwBar.mm.o.d"
+  "/Users/olaslaptop/learningss/Ccode/opengl/oglcreation/tutorial1/external/AntTweakBar-ObjectiveC/TwColors.mm" "external/CMakeFiles/ANTTWEAKBAR_116_OGLCORE_GLFW.dir/AntTweakBar-ObjectiveC/TwColors.mm.o" "gcc" "external/CMakeFiles/ANTTWEAKBAR_116_OGLCORE_GLFW.dir/AntTweakBar-ObjectiveC/TwColors.mm.o.d"
+  "/Users/olaslaptop/learningss/Ccode/opengl/oglcreation/tutorial1/external/AntTweakBar-ObjectiveC/TwFonts.mm" "external/CMakeFiles/ANTTWEAKBAR_116_OGLCORE_GLFW.dir/AntTweakBar-ObjectiveC/TwFonts.mm.o" "gcc" "external/CMakeFiles/ANTTWEAKBAR_116_OGLCORE_GLFW.dir/AntTweakBar-ObjectiveC/TwFonts.mm.o.d"
+  "/Users/olaslaptop/learningss/Ccode/opengl/oglcreation/tutorial1/external/AntTweakBar-ObjectiveC/TwMgr.mm" "external/CMakeFiles/ANTTWEAKBAR_116_OGLCORE_GLFW.dir/AntTweakBar-ObjectiveC/TwMgr.mm.o" "gcc" "external/CMakeFiles/ANTTWEAKBAR_116_OGLCORE_GLFW.dir/AntTweakBar-ObjectiveC/TwMgr.mm.o.d"
+  "/Users/olaslaptop/learningss/Ccode/opengl/oglcreation/tutorial1/external/AntTweakBar-ObjectiveC/TwOpenGL.mm" "external/CMakeFiles/ANTTWEAKBAR_116_OGLCORE_GLFW.dir/AntTweakBar-ObjectiveC/TwOpenGL.mm.o" "gcc" "external/CMakeFiles/ANTTWEAKBAR_116_OGLCORE_GLFW.dir/AntTweakBar-ObjectiveC/TwOpenGL.mm.o.d"
+  "/Users/olaslaptop/learningss/Ccode/opengl/oglcreation/tutorial1/external/AntTweakBar-ObjectiveC/TwOpenGLCore.mm" "external/CMakeFiles/ANTTWEAKBAR_116_OGLCORE_GLFW.dir/AntTweakBar-ObjectiveC/TwOpenGLCore.mm.o" "gcc" "external/CMakeFiles/ANTTWEAKBAR_116_OGLCORE_GLFW.dir/AntTweakBar-ObjectiveC/TwOpenGLCore.mm.o.d"
+  "/Users/olaslaptop/learningss/Ccode/opengl/oglcreation/tutorial1/external/AntTweakBar-ObjectiveC/TwPrecomp.mm" "external/CMakeFiles/ANTTWEAKBAR_116_OGLCORE_GLFW.dir/AntTweakBar-ObjectiveC/TwPrecomp.mm.o" "gcc" "external/CMakeFiles/ANTTWEAKBAR_116_OGLCORE_GLFW.dir/AntTweakBar-ObjectiveC/TwPrecomp.mm.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
